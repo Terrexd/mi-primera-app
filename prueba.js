6052906b01dbe7a0f94ed297e687f9c6b03d589d
Terrexd <javiervalenciamiranda1@gmail.com>
2026-09-21 10:15:33 +0200

    Season 1

---
+++ prueba.js
@@ -0,0 +1,67 @@
+/*
+    EJERCICIO 1: 
+    let modulo = "Desarrollo de Interfaces";
+    let horas = 140;
+    const IVA = 0.21;
+    let aprobado = true;
+
+    console.log(modulo + " tiene " + horas + " horas")
+
+    EJERCICIO 2:
+    let edad = 20;
+    edad = 21;
+    console.log(edad)
+
+    EJERCICIO 3:
+    /*
+function doble(n){
+    return n * 2;
+}
+
+function esMayorDeEdad(edad) { 
+ return edad >= 18; 
+} 
+
+function saludar(nombre) { 
+ return "Hola " + nombre; 
+} 
+
+
+    Segunda forma:
+
+    doble = (n) => { 
+    return n * 2; 
+    } 
+
+    esMayorDeEdad = (edad) =>{ 
+    return edad >= 18; 
+    } 
+
+    saludar = (nombre) => { 
+    return "Hola " + nombre; 
+    } 
+
+    console.log(doble(2));
+    console.log(esMayorDeEdad(17))
+    console.log(saludar("Ana"))
+
+    areaRectangulo = (base,altura) =>{
+        return base * altura
+    }
+
+    console.log(areaRectangulo(2,3))
+
+    EJERCICIO 4:
+
+*/
+
+const alumno = {
+    nombre: "Javier",
+    edad: 21,
+    ciclo: "2º DAM",
+    modulos: ["Desarrollo de Interfaces", "Acceso a Datos", "Android"],
+}
+
+alumno.edad = 21;
+alumno.modulos.push("Inglés");
+alumno.modulos.length
